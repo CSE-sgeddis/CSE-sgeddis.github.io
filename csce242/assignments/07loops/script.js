d24e264353d09876013a66ed282654a301f80cae
CSE-sgeddis <shalindixon@gmail.com>
2025-09-23 17:27:04 -0400

    added loops assignment

---
+++ csce242/assignments/07loops/script.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById("drawBtn");
+const scene = document.getElementById("scene");
+
+btn.addEventListener("click", () => {
+    scene.innerHTML = "";
+
+    // Draw 6 clouds
+    for (let i=0; i < 6; i++){
+        const cloud = document.createElement("div");
+        cloud.className = "cloud";
+        scene.appendChild(cloud);
+    }
+
+    // Draw 6 trees
+    for (let i=0; i < 6; i++){
+        const tree = document.createElement("div");
+        tree.className = "tree";
+        scene.appendChild(tree);
+    }
+
+    // Morning or Night by time
+    const hour = new Date().getHours();
+    let skyObject;
+    if (hour >= 18 || hour < 6) {
+        document.body.style.background = "black";
+        skyObject = document.createElement("div");
+        skyObject.className = "moon";
+    } else{
+        document.body.style.background = "skyblue";
+        skyObject = document.createElement("div");
+        skyObject.className = "sun";
+    }
+    scene.appendChild(skyObject);
+})
